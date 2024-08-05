ee0ee2a751f389c9ce7fd5b220a9bfd6e061b46b
unknown <abhishekmishra992016@gmail.com>
2024-08-05 09:34:38 +0530

    model changes

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ app.use(bodyParser.json({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const imageDirectoryPath = path.join(
-  __dirname,
-  "../",
-  "COMPARECSV_FILES",
-  "OmrImages",
-  "Images_2024-05-04T04-38-30-972Z/005.jpg"
+    __dirname,
+    "../",
+    "COMPARECSV_FILES",
+    "OmrImages",
+    "Images_2024-05-04T04-38-30-972Z/005.jpg"
 );
 // Serve static files from the 'extractedFiles' directory
 app.use("/images", express.static(imageDirectoryPath));
@@ -46,95 +46,95 @@ app.use(templeteRoutes);
 
 // Define associations with cascading deletes
 Templete.hasMany(MetaData, {
-  foreignKey: {
-    name: "templeteId",
-    allowNull: false,
-  },
-  onUpdate: "CASCADE",
+    foreignKey: {
+        name: "templeteId",
+        allowNull: false,
+    },
+    onUpdate: "CASCADE",
 });
 
 MetaData.belongsTo(Templete, {
-  foreignKey: {
-    name: "templeteId",
-    allowNull: false,
-  },
-  onUpdate: "CASCADE",
+    foreignKey: {
+        name: "templeteId",
+        allowNull: false,
+    },
+    onUpdate: "CASCADE",
 });
 
 Templete.hasMany(Files, {
-  foreignKey: {
-    allowNull: false,
-  },
-  onUpdate: "CASCADE",
+    foreignKey: {
+        allowNull: false,
+    },
+    onUpdate: "CASCADE",
 });
 
 Files.belongsTo(Templete, {
-  foreignKey: {
-    allowNull: false,
-  },
-  onUpdate: "CASCADE",
+    foreignKey: {
+        allowNull: false,
+    },
+    onUpdate: "CASCADE",
 });
 
 Assigndata.hasMany(RowIndexData, {
-  foreignKey: {
-    allowNull: false,
-  },
-  onUpdate: "CASCADE",
+    foreignKey: {
+        allowNull: false,
+    },
+    onUpdate: "CASCADE",
 });
 
 RowIndexData.belongsTo(Assigndata, {
-  foreignKey: {
-    allowNull: false,
-  },
-  onUpdate: "CASCADE",
+    foreignKey: {
+        allowNull: false,
+    },
+    onUpdate: "CASCADE",
 });
 
 Templete.hasMany(ImageDataPath, {
-  foreignKey: {
-    name: "templeteId",
-    allowNull: false,
-  },
-  onUpdate: "CASCADE",
+    foreignKey: {
+        name: "templeteId",
+        allowNull: false,
+    },
+    onUpdate: "CASCADE",
 });
 
 ImageDataPath.belongsTo(Templete, {
-  foreignKey: {
-    name: "templeteId",
-    allowNull: false,
-  },
-  onUpdate: "CASCADE",
+    foreignKey: {
+        name: "templeteId",
+        allowNull: false,
+    },
+    onUpdate: "CASCADE",
 });
 
 UpdatedData.belongsTo(User, {
-  foreignKey: {
-    name: "userId",
-    allowNull: false,
-  },
-  onUpdate: "CASCADE",
+    foreignKey: {
+        name: "userId",
+        allowNull: false,
+    },
+    onUpdate: "CASCADE",
 });
 
 User.hasMany(UpdatedData, {
-  foreignKey: {
-    name: "userId",
-    allowNull: false,
-  },
-  onUpdate: "CASCADE",
+    foreignKey: {
+        name: "userId",
+        allowNull: false,
+    },
+    onUpdate: "CASCADE",
 });
 
 Templete.hasMany(MappedData, {
-  foreignKey: {
-    name: "templeteId",
-    allowNull: false,
-  },
-  onUpdate: "CASCADE",
+    foreignKey: {
+        name: "templeteId",
+        allowNull: false,
+    },
+    onUpdate: "CASCADE",
 });
 
 MappedData.belongsTo(Templete, {
-  foreignKey: {
-    name: "templeteId",
-    allowNull: false,
-  },
-  onUpdate: "CASCADE",
+    foreignKey: {
+        name: "templeteId",
+        allowNull: false,
+    },
+    onUpdate: "CASCADE",
 });
 
 // app.listen(PORT, () => {
@@ -153,32 +153,32 @@ MappedData.belongsTo(Templete, {
 //   });
 
 sequelize
-  .sync({ force: false })
-  .then(async () => {
-    // Check if the admin user table exists, if not, create it
-    const adminUser = await User.findOne({ where: { role: "admin" } });
-    const hashedPassword = await bcrypt.hash("123456", 12);
-    if (!adminUser) {
-      await User.create({
-        userName: "admin",
-        mobile: "1234567891",
-        password: hashedPassword,
-        role: "Admin",
-        email: "admin@gmail.com",
-        permissions: {
-          dataEntry: true,
-          comparecsv: true,
-          csvuploader: true,
-          createTemplate: true,
-          resultGenerator: true,
-        },
-      });
-    }
-    // Start the server
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+    .sync({ force: false })
+    .then(async () => {
+        // Check if the admin user table exists, if not, create it
+        const adminUser = await User.findOne({ where: { role: "admin" } });
+        const hashedPassword = await bcrypt.hash("123456", 12);
+        if (!adminUser) {
+            await User.create({
+                userName: "admin",
+                mobile: "1234567891",
+                password: hashedPassword,
+                role: "Admin",
+                email: "admin@gmail.com",
+                permissions: {
+                    dataEntry: true,
+                    comparecsv: true,
+                    csvuploader: true,
+                    createTemplate: true,
+                    resultGenerator: true,
+                },
+            });
+        }
+        // Start the server
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Unable to connect to the database:", err);
     });
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
